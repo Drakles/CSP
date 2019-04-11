4b8ff59eb2df19a15f06a269597ef6c8f83751ed
Kajetan <drakles1@gmail.com>
2019-04-11 14:02:49 +0200

    start implementing degreeHeuristic

--- src/main/java/task2/csp/solver/heuristic/DegreeHeuristic.java
+++ src/main/java/task2/csp/solver/heuristic/DegreeHeuristic.java
@@ -1,5 +1,6 @@
 package task2.csp.solver.heuristic;
 
+import java.util.Comparator;
 import java.util.List;
 import task2.boardgame.SquareBoardGame;
 import task2.csp.Assigment;
@@ -10,6 +11,24 @@ public class DegreeHeuristic implements Heuristic {
   @Override
   public Variable choose(
       List<Variable> variableList, List<Assigment> assigments, SquareBoardGame game) {
+    // chose the one with the most constraints among remaining variables
+//    return variableList
+//        .parallelStream()
+//        .max(Comparator.comparingInt(v -> numberOfOccurences(v, game.getConstraints())))
+//        .orElse(null);
     return null;
+  }
+
+//  private int numberOfOccurences(Variable chosen, List<Variable[]> constraints) {
+//    return (int)
+//        constraints
+//            .parallelStream()
+//            .filter(constraint -> chosen.equals(constraint[0]) || chosen.equals(constraint[1]))
+//            .count();
+//  }
+
+  @Override
+  public String toString() {
+    return "DegreeHeuristic";
   }
 }
